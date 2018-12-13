9235fe5bf3c36d8bf509db3c58ad48a3815a5e3b
Erik Scheetz <erikscheetz@Eriks-MacBook-Pro.local>
2018-12-13 15:16:34 -0700

    edits

--- src/components/ImageGenerator/ImageGenerator.js
+++ src/components/ImageGenerator/ImageGenerator.js
@@ -3,7 +3,7 @@ import { View, Image } from 'react-native'
 
   const ImageGenerator = ({recipe}) => (
      <Image source = {{uri: recipe.image_url}}
-     style = {{ width: 200, height: 200 }}
+     style = {{ width: 128, height: 128 }}
      />
   )
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,35 +1,51 @@
 // import library to help create a component
-import React from 'react'
+import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-
+import { Header, Left, Body, Right, Button, Icon, Title } from 'native-base'
 
 // create a component
-const NavBar = (props) => {
-  const {textStyle, viewStyle} = styles
-  return (
-    <View style={viewStyle}>
-      <Text style={textStyle}> Shef </Text>
-    </View>
-  )
-}
+class NavBar extends Component {
+  render(){
+    const styles = {
+      viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: 90,
+        paddingBottom: 15,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2,  },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+      },
+      textStyle: {
+        fontSize: 20
+      }
+    }
+    const {textStyle, viewStyle} = styles
 
-const styles = {
-  viewStyle: {
-    backgroundColor: '#F8F8F8',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    height: 90,
-    paddingBottom: 15,
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2,  },
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative'
-  },
-  textStyle: {
-    fontSize: 20
+    return (
+      <Header>
+        <Left>
+          <Button transparent>
+            <Icon name='menu' />
+          </Button>
+        </Left>
+        <Body>
+          <Title style={textStyle}> Shef </Title>
+        </Body>
+        <Right>
+          <Button transparent>
+            <Icon name='profile' />
+          </Button>
+        </Right>
+      </Header>
+    )
   }
 }
+
+
 
 // make component available to other parts of the app
 export default NavBar
