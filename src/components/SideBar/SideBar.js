ba31c693a171c9cf5106663ef0f99901633d2c30
Erik Scheetz <erikscheetz@Eriks-MacBook-Pro.local>
2018-12-13 15:49:11 -0700

    Sidebar edits

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { AppRegistry, Image, StatusBar } from "react-native";
 import { Container, Content, Text, List, ListItem } from "native-base";
-const routes = ["Home", "Chat", "Profile"];
 export default class SideBar extends React.Component {
   render() {
     return (
