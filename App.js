651d8c5b7da487fc21b88ba246c1b74393c8fa08
Riley Burns <rileyburns345@gmail.com>
2018-12-14 13:41:30 -0700

    merge prep

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Container, Header, Content } from 'native-base'
 import NavBar from './src/components/NavBar/NavBar'
 import RecipeList from './src/components/RecipeList/RecipeList'
 import SingleCardView from './src/components/SingleCardView/SingleCardView'
+import LoginSignup from './src/components/LoginSignup/LoginSignup'
 
 const API = process.env.API || 'http://localhost:3000'
 
@@ -13,7 +14,8 @@ export default class App extends Component {
     super(props)
     this.state = {
       recipes: [],
-      singleView: false
+      singleView: false,
+      token: ''
     }
   }
 
@@ -44,15 +46,40 @@ export default class App extends Component {
       })
   }
 
+
+  loginClick = async (loginInfo) => {
+    // console.log('LOGIN CLICK', username, password);
+
+    const response = await fetch(`${API}/sign-in`, {
+     method: 'POST',
+     headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify(loginInfo)
+    })
+    console.log('json: ', response);
+    if(response.status === 200) {
+    const json = await response.json()
+    this.setState({
+      ...this.state,
+      token: JSON.parse(response._bodyInit).token
+    })
+    console.log(this.state.token);
+    }
+  }
+
   render() {
     console.log("here", this.state)
     return (
       <Container >
         <NavBar />
         <Content>
-          {this.state.singleView ? <SingleCardView card={this.state.singleView}/> : null}
+          {this.state.token ? null : <LoginSignup loginClick={this.loginClick}/>}
+          {this.state.singleView ? <SingleCardView card={this.state.singleView} backClick={this.backClick}/> : null}
           {this.state.singleView ? null : <RecipeList recipes={this.state.recipes} cardClick={this.cardClick}/>}
         </Content>
+
+
       </Container>
 
     );
